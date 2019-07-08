0f7b787df22819e49d4c84982e044bb5af708807
Lucas Nogueira <lucas_snogueira@hotmail.com>
2019-07-08 10:12:38 -0300

    add models

--- src/app/pages/organization/commit-list/commit-list.component.ts
+++ src/app/pages/organization/commit-list/commit-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GithubService } from '../../../shared/service/github.service';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { SnackbarService } from '../../../shared/service/snackbar.service';
 
 @Component({
   selector: 'app-commit-list',
@@ -15,27 +16,44 @@ export class CommitListComponent implements OnInit, OnDestroy {
 
   constructor(
     private ghService: GithubService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private snackBarService: SnackbarService
   ) {}
 
   ngOnInit() {
     this.repoName = this.activatedRoute.snapshot.params.repo;
 
     this.subscription.add(
-      this.activatedRoute.parent.params
-        .subscribe(res => {
-          this.orgName = res.org;
-        })
+      this.activatedRoute.parent.params.subscribe(res => {
+        this.orgName = res.org;
+      })
+    );
+
+      //Do a fork join here
+
+    this.ghService
+    .getCommits(this.orgName, this.repoName)
+    .subscribe(
+      res => {
+        console.log('commit response', res.body);
+        console.log('commit header', res.headers.get('Link'));
+      },
+      rej => {
+        console.log('af', rej);
+      }
     );
 
     this.ghService
-      .getCommits(this.orgName, this.repoName)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(rej => {
-        console.log('af', rej);
-      });
+      .getBranches(this.orgName, this.repoName)
+      .subscribe(
+        res => {
+          console.log('commit response', res.body);
+          console.log('commit header', res.headers.get('Link'));
+        },
+        rej => {
+          console.log('af', rej);
+        }
+      );
   }
 
   ngOnDestroy() {
--- src/app/shared/service/github.service.ts
+++ src/app/shared/service/github.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpResponse } from '@angular/common/http';
 import { Repository } from '../model/repository.model';
 import { Observable } from 'rxjs';
 import { Organization } from '../model/organization.model';
+import { Commit } from '../model/commit.model';
+import { Branch } from '../model/branch.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,33 +13,37 @@ export class GithubService {
   private readonly URI = 'https://api.github.com';
   private headers = new HttpHeaders({
     'Content-Type': 'application/json',
-    'Accept': 'application/vnd.github.v3+json'
+    Accept: 'application/vnd.github.v3+json'
   });
 
   constructor(private http: HttpClient) {}
 
   getRepositories(org: string): Observable<Repository[]> {
-    return this.http
-      .get<Repository[]>(
-        `${this.URI}/orgs/${org}/repos?type=public`, { headers: this.headers }
-      );
+    return this.http.get<Repository[]>(
+      `${this.URI}/orgs/${org}/repos?type=public`,
+      { headers: this.headers }
+    );
   }
 
   getOrganization(org: string): Observable<Organization> {
-    return this.http
-      .get<Organization>(`${this.URI}/orgs/${org}`, { headers: this.headers });
+    return this.http.get<Organization>(`${this.URI}/orgs/${org}`, {
+      headers: this.headers
+    });
   }
 
-  getCommits(
-    owner: string,
-    repo: string,
-    branch = 'master'
-  ): Promise<any> {
+  getCommits(owner: string, repo: string, branch = 'master'): Observable<HttpResponse<Commit[]>> {
     return this.http
-      .get<any>(`${this.URI}/repos/${owner}/${repo}/commits?sha=${branch}`, {
-        headers: this.headers
-      })
-      .toPromise();
+      .get<Commit[]>(
+        `${this.URI}/repos/${owner}/${repo}/commits?sha=${branch}`, {
+          observe: 'response'
+      });
   }
 
+  getBranches(owner: string, repo: string): Observable<HttpResponse<Branch[]>> {
+    return this.http
+      .get<Branch[]>(
+        `${this.URI}/repos/${owner}/${repo}/branches`, {
+          observe: 'response'
+      });
+  }
 }
